a785667b72b2b98717907eb0612b38f71b380ebd
cbroh <cameron.rohan4@gmail.com>
2021-10-22 09:08:38 +0800

    GPGLL parse decimal length fix

--- GPGLLParse.py
+++ GPGLLParse.py
@@ -3,18 +3,22 @@ emptydata = "$GPGLL,,,,,,A,*1D\r\n"
 
 
 def gpgll_parse(sentence):
-	words = sentence.split(',')
-	if words[1] != '':
-		nmealat = words[1].split('.')
-		lat = int(nmealat[0][:-2]) + int(nmealat[0][-2:]) / 60 + int(nmealat[1]) / 6000
-		lat = lat * 2 * ((words[2] == 'N') - 0.5)
+    words = sentence.split(',')
+    if words[1] != '':
+        nmealat = words[1].split('.')
+        lat = int(nmealat[0][:-2]) + int(nmealat[0][-2:]) / 60 + int(nmealat[1]) / (60 * pow(10, len(nmealat[1])))
+        if words[2] == 'S':
+            lat = -lat
 
-		nmealon = words[3].split('.')
-		lon = int(nmealon[0][:-2]) + int(nmealon[0][-2:]) / 60 + int(nmealon[1]) / 6000
-		lon = lon * 2 * ((words[4] == 'E') - 0.5)
-		return lat, lon
-	return 0, 0
+        nmealon = words[3].split('.')
+        lon = int(nmealon[0][:-2]) + int(nmealon[0][-2:]) / 60 + int(nmealon[1]) / (60 * pow(10, len(nmealon[1])))
+        if words[4] == 'W':
+            lon = -lon
+
+        return lat, lon
+    return 0, 0
 
 
 print(gpgll_parse(withdata))
 print(gpgll_parse(emptydata))
+
