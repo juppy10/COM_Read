caaea9180fd2b30d979979bd602c2eeba08e036f
juppy10 <william.jupp@gmail.com>
2021-11-06 09:49:14 +0800

    Done

--- COM.py
+++ COM.py
@@ -1,15 +1,42 @@
 import serial
+import csv
+import codecs
+import time
 
-serialPort = serial.Serial(port = "COM11", baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+serialPort = serial.Serial(port="COM11", baudrate=9600, bytesize=8, timeout=10, stopbits=serial.STOPBITS_ONE)
 
-serialString = ""                           # Used to hold data coming over UART
+serialString = ""  # Used to hold data coming over UART
+count = 0
 
-while(1):
+with open('data.csv', 'w', newline='') as dataFile:
+    dataWriter = csv.writer(dataFile)
+    dataWriter.writerow(["time (ms)", "X-Axis Acc (G)", "Y-Axis Acc (G)", "Z-Axis Acc (G)", "ADC (mV)", "CAN ID", "CAN Data", "GNSS Data"])
+    start = time.time()
 
-    # Wait until there is data waiting in the serial buffer
-    if(serialPort.in_waiting > 0):
+while 1:
+    if serialPort.in_waiting > 0:
         # Read data out of the buffer until a carraige return / new line is found
-        serialString = serialPort.readline()
-        # Print the contents of the serial data
-        print(serialString.decode('Ascii'))
-        serialPort.write(b"Thank you for sending data \r\n")
+        serialStringX = serialPort.read(2)
+        serialStringY = serialPort.read(2)
+        serialStringZ = serialPort.read(2)
+        serialStringADC = serialPort.read(2)
+        CANID = serialPort.read(1)
+        CANData = serialPort.read(1)
+        GNSS_Length = int.from_bytes(serialPort.read(1), byteorder='big')
+        GNSS_Data = serialPort.read(GNSS_Length)
+        nowGx = (int.from_bytes(serialStringX, byteorder='big', signed=True) / 16384) - 0.229
+        nowGy = (int.from_bytes(serialStringY, byteorder='big', signed=True) / 16384) - 0.206
+        nowGz = (int.from_bytes(serialStringZ, byteorder='big', signed=True) / 16384) - 0.099
+        nowADC = int.from_bytes(serialStringADC, byteorder='big', signed=True) / 1.218
+        CANID = codecs.encode(CANID, "hex")
+        CANData = codecs.encode(CANData, "hex")
+        GNSS_Data = GNSS_Data.decode('Ascii')
+        count = count + 0.01
+        with open('data.csv', 'a', newline='') as dataFile:
+            dataWriter = csv.writer(dataFile)
+            dataWriter.writerow([str(count), str(nowGx), str(nowGy), str(nowGz), str(nowADC), str(CANID), str(CANData),GNSS_Data])
+            print(count)
+            if count > 10:
+                end = time.time()
+                print("Time:", end - start)
+                break
--- COM_Read.py
+++ COM_Read.py
@@ -3,6 +3,22 @@ import csv
 import codecs
 import time
 
+def gpgll_parse(sentence):
+    words = sentence.split(',')
+    if words[1] != '':
+        nmealat = words[1].split('.')
+        lat = int(nmealat[0][:-2]) + int(nmealat[0][-2:]) / 60 + int(nmealat[1]) / (60 * pow(10, len(nmealat[1])))
+        if words[2] == 'S':
+            lat = -lat
+
+        nmealon = words[3].split('.')
+        lon = int(nmealon[0][:-2]) + int(nmealon[0][-2:]) / 60 + int(nmealon[1]) / (60 * pow(10, len(nmealon[1])))
+        if words[4] == 'W':
+            lon = -lon
+
+        return lat, lon
+    return 0, 0
+
 serialPort = serial.Serial(port="COM11", baudrate=9600, bytesize=8, timeout=10, stopbits=serial.STOPBITS_ONE)
 
 serialString = ""  # Used to hold data coming over UART
@@ -10,7 +26,7 @@ count = 0
 
 with open('data.csv', 'w', newline='') as dataFile:
     dataWriter = csv.writer(dataFile)
-    dataWriter.writerow(["time (ms)", "X-Axis Acc (G)", "Y-Axis Acc (G)", "Z-Axis Acc (G)", "ADC (mV)", "CAN ID", "CAN Data", "GNSS Data"])
+    dataWriter.writerow(["time (ms)", "X-Axis Acc (G)", "Y-Axis Acc (G)", "Z-Axis Acc (G)", "ADC (mV)", "CAN ID", "CAN Data", "GNSS Data (lat)", "GNSS Data (lon)"])
     start = time.time()
 
 while 1:
@@ -24,19 +40,20 @@ while 1:
         CANData = serialPort.read(1)
         GNSS_Length = int.from_bytes(serialPort.read(1), byteorder='big')
         GNSS_Data = serialPort.read(GNSS_Length)
-        nowGx = (int.from_bytes(serialStringX, byteorder='big', signed=True) / 16384) - 0.192
-        nowGy = (int.from_bytes(serialStringY, byteorder='big', signed=True) / 16384) - 0.178
-        nowGz = (int.from_bytes(serialStringZ, byteorder='big', signed=True) / 16384) - 0.096
+        nowGx = (int.from_bytes(serialStringX, byteorder='big', signed=True) / 16384) - 0.14
+        nowGy = (int.from_bytes(serialStringY, byteorder='big', signed=True) / 16384) - 0.17
+        nowGz = (int.from_bytes(serialStringZ, byteorder='big', signed=True) / 16384) - 0.099
         nowADC = int.from_bytes(serialStringADC, byteorder='big', signed=True) / 1.218
         CANID = codecs.encode(CANID, "hex")
         CANData = codecs.encode(CANData, "hex")
         GNSS_Data = GNSS_Data.decode('Ascii')
-        count = count + 1
+        count = count + 0.01
+        [lat, lon] = gpgll_parse(GNSS_Data)
         with open('data.csv', 'a', newline='') as dataFile:
             dataWriter = csv.writer(dataFile)
-            dataWriter.writerow([str(count), str(nowGx), str(nowGy), str(nowGz), str(nowADC), str(CANID), str(CANData),GNSS_Data])
+            dataWriter.writerow([str(count), str(nowGx), str(nowGy), str(nowGz), str(nowADC), str(CANID), str(CANData), lat, lon])
             print(count)
-            if count > 5:
+            if count > 10:
                 end = time.time()
                 print("Time:", end - start)
                 break
